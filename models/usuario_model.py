b57a620b423790d4f6bd57d28f338c0a4de88c46
Melf17 <mel17_@hotmail.com>
2024-06-22 20:32:58 -0300

    Usuario model

---
+++ models/usuario_model.py
@@ -0,0 +1,13 @@
+from config.database import Base
+from sqlalchemy import Column, Integer, String
+
+class UsuarioModel(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key = True)
+    nombre = Column(String(30))
+    email = Column(String(50))
+    rol = Column(String(20))
+    password = Column(String(20))
+
+
