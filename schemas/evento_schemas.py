3ead6d29694fd4909e91b7dc023c967b771253ef
monsepronello <monsepronello@gmail.com>
2024-06-22 18:15:04 -0700

    jjj

---
+++ schemas/evento_schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from datetime import date
+
+
+class Evento(BaseModel):
+    id: int = Field(gt=0)
+    nombre: str = Field(min_length=1, max_length=35)
+    descripcion: str= Field(min_lenght=1, max_length=160)
+    fecha_inicio: date 
+    fecha_fin: date
+    lugar: str
+    cupos: int
+    category:int
